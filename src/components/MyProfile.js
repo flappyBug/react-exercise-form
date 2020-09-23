69ca67643b9893d3adbc0be7c329eb32370518d4
Hourann <tdkgn@outlook.com>
2020-09-23 11:27:19 +0800

    [hourann] feat: implement html

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,14 +1,30 @@
-import React, {Component} from 'react';
-import './myProfile.less';
+import React, { Component } from "react";
+import "./myProfile.less";
 
 class MyProfile extends Component {
   render() {
     return (
-      <form></form>
+      <form>
+        <h1>My Profile</h1>
+        <label for="name">Name</label>
+        <input name="name" id="name" placeholder="Your name"></input>
+        <label for="gender">Gender</label>
+        <select name="gender" id="gender">
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+        <label for="description">Description</label>
+        <textarea
+          name="description"
+          id="description"
+          placeholder="Description about yourself"
+        />
+        <input type="checkbox" name="agree" id="agree" />
+        <label for="agree">I have read the terms of conduct</label>
+        <button>Submmit</button>
+      </form>
     );
   }
 }
 
 export default MyProfile;
-
-
