b2f9ee81a9f0fbd17d3bbe3380f13c335d02d1ff
Hourann <tdkgn@outlook.com>
2020-09-23 16:35:08 +0800

    [hourann] feat: add form validation

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -2,28 +2,67 @@ import React, { Component } from "react";
 import "./myProfile.less";
 
 class MyProfile extends Component {
+  state = {
+    description: "",
+    gender: "male",
+    name: "",
+    agree: false,
+  };
+
+  onChange = (event) => {
+    // event.preventDefault();
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    this.setState({
+      [target.name]: value,
+    });
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
   render() {
     return (
       <form>
         <h1>My Profile</h1>
-        <label for="name">Name</label>
-        <input name="name" id="name" placeholder="Your name"></input>
-        <label for="gender">Gender</label>
-        <select name="gender" id="gender">
+        <label htmlFor="name">Name</label>
+        <input
+          name="name"
+          id="name"
+          placeholder="Your name"
+          onChange={this.onChange}
+        ></input>
+        <label htmlFor="gender">Gender</label>
+        <select name="gender" id="gender" onChange={this.onChange}>
           <option value="male">Male</option>
           <option value="female">Female</option>
         </select>
-        <label for="description">Description</label>
+        <label htmlFor="description">Description</label>
         <textarea
           name="description"
           id="description"
           placeholder="Description about yourself"
+          onChange={this.onChange}
         />
         <div>
-          <input type="checkbox" name="agree" id="agree" />
-          <label for="agree">I have read the terms of conduct</label>
+          <input
+            type="checkbox"
+            name="agree"
+            id="agree"
+            onChange={this.onChange}
+          />
+          <label htmlFor="agree">I have read the terms of conduct</label>
         </div>
-        <button>Submmit</button>
+        <button
+          disabled={
+            !(this.state.description && this.state.name && this.state.agree)
+          }
+          onClick={this.onSubmit}
+        >
+          Submit
+        </button>
       </form>
     );
   }
